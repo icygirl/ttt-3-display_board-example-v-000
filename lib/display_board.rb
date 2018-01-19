a4c4fe65c8e86e29a43c5313366424a703dcbe68
Virginia Herron <inadream@gmail.com>
2018-01-19 02:42:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  cell = '"   "'
+  cell = "   "
   pipe = "   |   "
   seperator = "-----------"
   print cell, pipe, cell, pipe, cell
